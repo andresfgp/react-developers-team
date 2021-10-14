4782fa3854263c1d9e03e41f9dc2813d0a11232a
jsmarinn <jsmarinn@gmail.com>
2021-10-13 23:56:46 -0500

    .gitignore

--- ApiNodeJS/config/database.js
+++ ApiNodeJS/config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "@dmin123_*",
   database: process.env.DB_DATABASE || "store",
   host: process.env.DB_HOST || "localhost",
   dialect: process.env.DB_DIALECT || "mysql",
